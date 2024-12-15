1247ebf1c0fc504ca811a6c32a78cb452f73f534
gki-webik <gki-webik@yandex.ru>
2024-12-15 11:38:22 +0600

    add structure

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,19 @@
-// https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: '2024-11-01',
-  devtools: { enabled: true }
-})
+  app: {
+    head: {
+      title: "Главная страница",
+      titleTemplate: "%s - Memka",
+      meta: [
+        { charset: "utf-8" },
+        { name: "viewport", content: "width=device-width, initial-scale=1" },
+        { name: "robots", content: "index,follow" },
+      ],
+    },
+  },
+  css: [],
+  components: true,
+  devServer: {
+    port: 3000,
+  },
+  compatibilityDate: "2024-12-15",
+});
