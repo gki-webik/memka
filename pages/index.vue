5990ec165b574c9a71d6d40382e7c181f778b096
gki-webik <gki-webik@yandex.ru>
2024-12-15 17:48:05 +0600

    add adaptive block2

--- pages/index.vue
+++ pages/index.vue
@@ -10,7 +10,7 @@
               СРЕДИ ПОДПИСЧИКОВ  И ПРИВЛЕКИТЕ НОВЫХ ИНВЕСТОРОВ
             </span>
           </h1>
-          <a href="" class="lets_go">
+          <a href="#b2" class="lets_go">
             <span class="is-1"></span>ВПЕРЕД <span class="is-2"></span>
           </a>
         </div>
@@ -20,7 +20,25 @@
         </div>
       </div>
     </div>
-    <div class="block2"></div>
+    <div class="block2" id="b2">
+      <h2><span class="is-line">Ме</span>мы</h2>
+      <div class="items">
+        <div class="item is-1">
+          <div class="text">
+            ВЫДЕЛЯЙТЕСЬ ИЗ ТОЛПЫ И <br />
+            ПРИВЛЕКАЙТЕ БОЛЬШЕ
+          </div>
+          <img src="/assets/images/3.png" alt="" loading="lazy" />
+        </div>
+        <div class="item is-2">
+          <div class="text is-right">
+            НОВЫХ ПОДПИСЧИКОВ И <br />
+            ПОТЕНЦИАЛЬНЫХ КЛИЕНТОВ
+          </div>
+          <img src="/assets/images/4.png" alt="" loading="lazy" />
+        </div>
+      </div>
+    </div>
   </main>
 </template>
 
