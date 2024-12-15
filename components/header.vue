b5f55b6f54dfe6efc2d5ae927feebd317883cbd9
gki-webik <gki-webik@yandex.ru>
2024-12-15 11:59:20 +0600

    del dist in gitignore

--- components/header.vue
+++ components/header.vue
@@ -1,6 +1,17 @@
 <template>
   <div>
-    <header>header</header>
+    <header>
+      <div class="logo"><img src="" alt="Логотип" /></div>
+      <nav>
+        <a href="">Анимация</a>
+        <a href="">Иллюстрация</a>
+        <a href="">Мемы</a>
+        <a href="">Стикеры</a>
+        <a href="">Сайты</a>
+        <a href="">Реклама</a>
+        <a href="">Контакты</a>
+      </nav>
+    </header>
   </div>
 </template>
 
