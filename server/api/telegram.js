b81f99d035ff4811eca35a4e8b7ff45e0732dbbf
gki-webik <gki-webik@yandex.ru>
2024-12-19 22:57:57 +0600

    upd bot

--- server/api/telegram.js
+++ server/api/telegram.js
@@ -2,34 +2,37 @@
 import { defineEventHandler } from 'h3';
 import TelegramBot from 'node-telegram-bot-api';
 
-const bot = new TelegramBot('7600941340:AAF6MjBwenwZCiFUHkWIVZf7hAcYnHZu18Y', { polling: true });
+// Проверяем, существует ли уже экземпляр бота
+if (!global.bot) {
+  global.bot = new TelegramBot('7600941340:AAF6MjBwenwZCiFUHkWIVZf7hAcYnHZu18Y', { polling: true });
 
-// Отправка сообщения при инициализации бота
-bot.sendMessage("6317166538", 'Вы запросили мем с параметрами big 9.');
+  // Отправка сообщения при инициализации бота
+  global.bot.sendMessage("6317166538", 'Вы запросили мем с параметрами big 9.');
 
-bot.onText(/\/start/, (msg) => {
-  const chatId = msg.chat.id;
-  bot.sendMessage(chatId, 'Добро пожаловать! Это стартовая команда.');
-});
+  global.bot.onText(/\/start/, (msg) => {
+    const chatId = msg.chat.id;
+    global.bot.sendMessage(chatId, 'Добро пожаловать! Это стартовая команда.');
+  });
 
-bot.onText(/\/test/, (msg) => {
-  const chatId = msg.chat.id;
-  bot.sendMessage(chatId, 'Вы использовали команду /test.');
-});
+  global.bot.onText(/\/test/, (msg) => {
+    const chatId = msg.chat.id;
+    global.bot.sendMessage(chatId, 'Вы использовали команду /test.');
+  });
 
-bot.onText(/\/mem big 9/, (msg) => {
-  const chatId = msg.chat.id;
-  bot.sendMessage(chatId, 'Вы запросили мем с параметрами big 9.');
-});
+  global.bot.onText(/\/mem big 9/, (msg) => {
+    const chatId = msg.chat.id;
+    global.bot.sendMessage(chatId, 'Вы запросили мем с параметрами big 9.');
+  });
 
-function createInvoice(chatId) {
-  bot.sendMessage(chatId, 'Счет на оплату создан.');
+  function createInvoice(chatId) {
+    global.bot.sendMessage(chatId, 'Счет на оплату создан.');
+  }
+
+  global.bot.onText(/\/invoice/, (msg) => {
+    const chatId = msg.chat.id;
+    createInvoice(chatId);
+  });
 }
-
-bot.onText(/\/invoice/, (msg) => {
-  const chatId = msg.chat.id;
-  createInvoice(chatId);
-});
 
 export default defineEventHandler((event) => {
   return 'Бот работает';
