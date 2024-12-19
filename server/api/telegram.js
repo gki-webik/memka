c2fa0f2b20435eac0a7b2c3b5b891ea2c1653979
gki-webik <gki-webik@yandex.ru>
2024-12-19 23:29:25 +0600

    upd

--- server/api/telegram.js
+++ server/api/telegram.js
@@ -1,23 +1,18 @@
 import { defineEventHandler, readBody } from 'h3';
 import TelegramBot from 'node-telegram-bot-api';
 
-// Ваш токен бота
 const botToken = '7600941340:AAF6MjBwenwZCiFUHkWIVZf7hAcYnHZu18Y';
-
-// URL вашего вебхука
 const webhookUrl = 'https://memka.vercel.app/api/telegram';
-
 const bot = new TelegramBot(botToken, { webHook: true });
 
 bot.setWebHook(webhookUrl);
 
-// Обработчик команды /start
 bot.onText(/\/start/, (msg) => {
   const chatId = msg.chat.id;
   const options = {
     reply_markup: {
       keyboard: [
-        ['/start', '/test'],
+        ['/start', '/invoice'],
         ['/try']
       ],
       resize_keyboard: true,
@@ -27,25 +22,23 @@ bot.onText(/\/start/, (msg) => {
   bot.sendMessage(chatId, 'Добро пожаловать! Чем я могу тебе помочь?', options);
 });
 
-// Обработчик команды /test
-bot.onText(/\/test/, (msg) => {
+bot.onText(/\/invoice/, (msg) => {
   const chatId = msg.chat.id;
-  bot.sendMessage(chatId, 'Вы использовали команду /test.');
-});
+  const title = 'Пример товара';
+  const description = 'Описание товара';
+  const payload = 'payload';
+  const providerToken = '';
+  const startParameter = 'start';
+  const currency = 'XTR';
+  const prices = [{ label: 'Цена', amount: 3 }];
 
-// Обработчик команды /try
-bot.onText(/\/try/, (msg) => {
-  const chatId = msg.chat.id;
-  bot.sendMessage(chatId, 'Вы использовали команду /try.');
+  bot.sendInvoice(chatId, title, description, payload, providerToken, startParameter, currency, prices);
 });
 
 export default defineEventHandler(async (event) => {
   try {
     const body = await readBody(event);
-
-    // Обработка входящих обновлений
     bot.processUpdate(body);
-
     return 'OK';
   } catch (error) {
     console.error('Ошибка обработки обновления:', error);
