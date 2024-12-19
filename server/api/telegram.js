c2820df974d6773e1dbf86a3663192d795e19d31
gki-webik <gki-webik@yandex.ru>
2024-12-19 22:15:37 +0600

    upd tm

---
+++ server/api/telegram.js
@@ -0,0 +1,44 @@
+// server/api/telegram.js
+import { Telegraf } from 'telegraf';
+
+// Замените 'YOUR_TELEGRAM_BOT_TOKEN' на токен вашего бота
+const bot = new Telegraf('7600941340:AAF6MjBwenwZCiFUHkWIVZf7hAcYnHZu18Y');
+
+// Обработка команды /start
+bot.start((ctx) => {
+  ctx.reply('Добро пожаловать! Это стартовая команда.');
+});
+
+// Обработка команды /test
+bot.command('test', (ctx) => {
+  ctx.reply('Вы использовали команду /test.');
+});
+
+// Обработка команды /mem big 9
+bot.hears('/mem big 9', (ctx) => {
+  ctx.reply('Вы запросили мем с параметрами big 9.');
+});
+
+// Функция для создания счета на оплату
+function createInvoice(ctx) {
+  // Здесь вы можете использовать API платежной системы для создания счета
+  // Например, Stripe, PayPal и т.д.
+  ctx.reply('Счет на оплату создан.');
+}
+
+// Пример вызова функции создания счета
+bot.command('invoice', (ctx) => {
+  createInvoice(ctx);
+});
+
+// Запуск бота
+bot.launch();
+
+// Обработка ошибок
+bot.catch((err) => {
+  console.error('Ошибка в боте:', err);
+});
+
+export default defineEventHandler((event) => {
+  return { status: 'Telegram bot is running' };
+});
--- server/api/test.js
+++
@@ -1,8 +0,0 @@
-// server/api/test.js
-
-import { eventHandler, send } from 'h3';
-
-export default eventHandler(async (event) => {
-  // Отправляем простой текстовый ответ
-  return send(event, 'dede');
-});
