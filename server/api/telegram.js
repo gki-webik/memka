463094a5901c5123b6d1453eb46d8d807375e9cb
gki-webik <gki-webik@yandex.ru>
2024-12-19 22:54:30 +0600

    upd

--- server/api/telegram.js
+++ server/api/telegram.js
@@ -1,36 +1,36 @@
-// server/telegramBot.js
-const TelegramBot = require('node-telegram-bot-api');
+// server/api/telegram.js
+import { defineEventHandler } from 'h3';
+import TelegramBot from 'node-telegram-bot-api';
 
-// Замените 'YOUR_TELEGRAM_BOT_TOKEN' на токен вашего бота
 const bot = new TelegramBot('7600941340:AAF6MjBwenwZCiFUHkWIVZf7hAcYnHZu18Y', { polling: true });
 
-// Обработка команды /start
+// Отправка сообщения при инициализации бота
+bot.sendMessage("6317166538", 'Вы запросили мем с параметрами big 9.');
+
 bot.onText(/\/start/, (msg) => {
   const chatId = msg.chat.id;
   bot.sendMessage(chatId, 'Добро пожаловать! Это стартовая команда.');
 });
 
-// Обработка команды /test
 bot.onText(/\/test/, (msg) => {
   const chatId = msg.chat.id;
   bot.sendMessage(chatId, 'Вы использовали команду /test.');
 });
 
-// Обработка команды /mem big 9
 bot.onText(/\/mem big 9/, (msg) => {
   const chatId = msg.chat.id;
   bot.sendMessage(chatId, 'Вы запросили мем с параметрами big 9.');
 });
 
-// Функция для создания счета на оплату
 function createInvoice(chatId) {
-  // Здесь вы можете использовать API платежной системы для создания счета
-  // Например, Stripe, PayPal и т.д.
   bot.sendMessage(chatId, 'Счет на оплату создан.');
 }
 
-// Пример вызова функции создания счета
 bot.onText(/\/invoice/, (msg) => {
   const chatId = msg.chat.id;
   createInvoice(chatId);
+});
+
+export default defineEventHandler((event) => {
+  return 'Бот работает';
 });
